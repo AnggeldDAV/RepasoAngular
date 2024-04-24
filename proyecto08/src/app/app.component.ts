3f3187c961b730101260b3d6cdffc5fcb954023e
AnggeldDAV <anggeld.adav@gmail.com>
2024-04-24 19:11:33 +0200

    proyecto08 avanzado falta funcionalidad validador y directivas

--- proyecto08/src/app/app.component.ts
+++ proyecto08/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ValidaNombreService } from './valida-nombre.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto08';
+  nombre: string = "";
+  mensaje: string = "";
+  Validador: ValidaNombreService;
+
+  constructor(ValNombre: ValidaNombreService) {
+    this.Validador = ValNombre;
+  }
+  valida() {
+    if (this.Validador.isValid(this.nombre)) {
+      return this.mensaje = `${this.nombre} es valido y correcto`;
+    } else {
+      return this.mensaje = `${this.nombre} no es valido y es incorrecto`;
+    }
+    
+  }
 }
